74e4b596c8c34492a7ff8b2465b8f7a7c8d756d8
Harry <tung.nguyenviet@orientsoftware.com>
2025-03-24 09:37:21 +0700

    Add favicon images and site web manifest; update .gitignore and SCSS layout; modify header and aside styles

--- assets/js/live-resume.js
+++ assets/js/live-resume.js
@@ -1,21 +1,98 @@
-$('[data-toggle="collapsible-nav"]').on('click', function(e){
-    var target = ($(this).data('target'));
-    $('#' + target).toggleClass('show');
+jQuery(function() {
+    setupCollapsibleNav();
+
+    setupHoverBoxEffect();
+
+    setupTypewriterEffect();
+
+    setupButtonShare();
+
+    setupScrollbar();
+
+    $('#last-updated').text(document.lastModified);
 });
 
-jQuery(function() {
+function setupScrollbar() {
+    $(window).on('scroll', function() {
+        var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+        var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+        var scrolled = (winScroll / height) * 100;
+        $('.scroll-indicator-bar').css('width', scrolled + '%');
+    });
+}
+
+function setupButtonShare() {
+    $('.btn-share').on('click', function(){
+        if (navigator.share) {
+            navigator.share({
+              title: 'NGUYỄN VIỆT TÙNG Resume',
+              url: location.href
+            })
+            .catch(console.error);
+        } else {
+            //fallback
+        }
+    });
+}
+
+function setupCollapsibleNav(){
+    $('[data-toggle="collapsible-nav"]').on('click', function(e){
+        var target = ($(this).data('target'));
+        $('#' + target).toggleClass('show');
+    });
+
     if(window.innerWidth >= 992) {
         $('#collapsible-nav').addClass('show');  //Show navigation menu in bigger screens by default.
     } else {
         $('#collapsible-nav').removeClass('show');
     }
+}
+
+function setupHoverBoxEffect() {
+    var classNames = ['in-up', 'in-right', 'in-down', 'in-left', 'out-up', 'out-right', 'out-down', 'out-left']; // Animation classes.
+
+    $('.hover-box').on('mouseenter', function (event) {
+            var direction = "up";
+        if(jQuery.fn.entry){ //Check if entry js file is loaded.
+            direction = $(this).entry({ e: event }); // Get mouse in direction.
+        }
+
+        $(this).removeClass(classNames.join(" ")); // Remove existing animation classes.
+        $(this).addClass("in-" + direction); //Add mouse in animation
+
+    }).on('mouseleave', function (event) {
+        var direction = "up";
+        if(jQuery.fn.entry){
+            direction = $(this).entry({ e: event }); // Get mouse out direction.
+        }
+
+        $(this).removeClass(classNames.join(" "));
+        $(this).addClass("out-" + direction); //Add mouse out animation
+
+    });
+
+    $(window).on('resize',
+        function() {
+            if ($('.hover-box').length) {
+                setHoverBoxPerspective();
+            }
+        }
+    );
 
     if ($('.hover-box').length) {
         setHoverBoxPerspective();
     }
+}
 
-    $('#last-updated').text(document.lastModified);
+function setHoverBoxPerspective() {
+    $('.hover-box').css({
+        'perspective': function () {
+            return Math.max( $(this).width(), $(this).height() ) * 2 + 50;
+        }
+    });
+}
 
+function setupTypewriterEffect() {
     let $typewriter = $('.typewriter-effect');
     if($typewriter.length) {
         var i = 0;
@@ -33,66 +110,4 @@ jQuery(function() {
         $typewriter.text('')
         typeWriter();
     }
-});
-
-$(window).on('resize',
-    function() {
-        if ($('.hover-box').length) {
-            setHoverBoxPerspective();
-        }
-    }
-);
-
-$(window).on('scroll', function() {
-    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    var scrolled = (winScroll / height) * 100;
-    $('.scroll-indicator-bar').css('width', scrolled + '%');
-});
-
-function setHoverBoxPerspective() {
-    $('.hover-box').css({
-        'perspective': function () {
-            return Math.max( $(this).width(), $(this).height() ) * 2 + 50;
-        }
-    });
 }
-
-var classNames = ['in-up', 'in-right', 'in-down', 'in-left', 'out-up', 'out-right', 'out-down', 'out-left']; // Animation classes.
-
-$('.hover-box').hover(
-    function (event) {
-        var direction = "up";
-        if(jQuery.fn.entry){ //Check if entry js file is loaded.
-            direction = $(this).entry({ e: event }); // Get mouse in direction.
-        }
-
-        $(this).removeClass(classNames.join(" ")); // Remove existing animation classes.
-        $(this).addClass("in-" + direction); //Add mouse in animation
-
-    }, 
-    
-    function (event) {
-
-        var direction = "up";
-        if(jQuery.fn.entry){
-            direction = $(this).entry({ e: event }); // Get mouse out direction.
-        }
-
-        $(this).removeClass(classNames.join(" "));
-        $(this).addClass("out-" + direction); //Add mouse out animation
-
-    }
-);
-
-$('.btn-share').on('click', function(){
-    if (navigator.share) {
-        navigator.share({
-          title: 'NGUYỄN VIỆT TÙNG Resume',
-          url: location.href
-        })
-        .catch(console.error);
-    } else {
-        //fallback
-    }
-});
