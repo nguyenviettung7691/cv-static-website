618d10b463c6ee2419ab78161083b7114868e029
Nguyen Viet Tung <nvtung.harry@gmail.com>
2026-06-14 20:57:38 +0700

    feat: update certification display and remove outdated PDF files

--- docs/.vitepress/theme/Cert.vue
+++ docs/.vitepress/theme/Cert.vue
@@ -3,28 +3,49 @@ import { onMounted, nextTick, computed } from 'vue'
 import { useData } from 'vitepress'
 const { localeIndex, isDark } = useData()
 
-const efsetUrl = computed(() => {
-  let lang = localeIndex.value != 'root' ? (localeIndex.value + '/') : '';
-  return `https://cert.efset.org/${lang}Yr64ri`
-});
-
 const darkSuffix = computed(() => isDark.value ? '.dark' : '')
 const certificates = computed(() => [
-  `https://certificates.dev/.netlify/images?url=https:%2F%2Fapi.certificates.dev%2Fcertificates%2Fthumbnail%2Fa035486e-e918-44ac-98fd-d6ef9a74bbc4${darkSuffix.value}.jpg`,
-  `https://certificates.dev/.netlify/images?url=https:%2F%2Fapi.certificates.dev%2Fcertificates%2Fthumbnail%2F9cf3a5b7-fd94-4a17-ab22-6d17596e6c56${darkSuffix.value}.jpg`,
-  `https://certificates.dev/.netlify/images?url=https:%2F%2Fapi.certificates.dev%2Fcertificates%2Fthumbnail%2Fa14392fa-86d6-4a2e-9154-32ccedf0f9af${darkSuffix.value}.jpg`,
-  `https://certificates.dev/.netlify/images?url=https:%2F%2Fapi.certificates.dev%2Fcertificates%2Fthumbnail%2Fa0444290-62ca-4f5c-bb46-90ed9228b6ef${darkSuffix.value}.jpg`,
-  `https://certificates.dev/.netlify/images?url=https:%2F%2Fapi.certificates.dev%2Fcertificates%2Fthumbnail%2F9f981598-89ce-4208-b545-8766a6e20fae${darkSuffix.value}.jpg`
+  {
+    file: `https://certificates.dev/.netlify/images?url=https:%2F%2Fapi.certificates.dev%2Fcertificates%2Fthumbnail%2Fa035486e-e918-44ac-98fd-d6ef9a74bbc4${darkSuffix.value}.jpg`,
+    url: 'https://certificates.dev/vuejs/certificates/a035486e-e918-44ac-98fd-d6ef9a74bbc4'
+  },
+  {
+    file: `https://certificates.dev/.netlify/images?url=https:%2F%2Fapi.certificates.dev%2Fcertificates%2Fthumbnail%2F9cf3a5b7-fd94-4a17-ab22-6d17596e6c56${darkSuffix.value}.jpg`,
+    url: 'https://certificates.dev/vuejs/certificates/9cf3a5b7-fd94-4a17-ab22-6d17596e6c56'
+  },
+  {
+    file: `https://certificates.dev/.netlify/images?url=https:%2F%2Fapi.certificates.dev%2Fcertificates%2Fthumbnail%2Fa14392fa-86d6-4a2e-9154-32ccedf0f9af${darkSuffix.value}.jpg`,
+    url: 'https://certificates.dev/javascript/certificates/a14392fa-86d6-4a2e-9154-32ccedf0f9af'
+  },
+  {
+    file: `https://certificates.dev/.netlify/images?url=https:%2F%2Fapi.certificates.dev%2Fcertificates%2Fthumbnail%2Fa0444290-62ca-4f5c-bb46-90ed9228b6ef${darkSuffix.value}.jpg`,
+    url: 'https://certificates.dev/javascript/certificates/a0444290-62ca-4f5c-bb46-90ed9228b6ef'
+  },
+  {
+    file: `https://certificates.dev/.netlify/images?url=https:%2F%2Fapi.certificates.dev%2Fcertificates%2Fthumbnail%2F9f981598-89ce-4208-b545-8766a6e20fae${darkSuffix.value}.jpg`,
+    url: 'https://certificates.dev/javascript/certificates/9f981598-89ce-4208-b545-8766a6e20fae'
+  },
 ])
 
 const microsoft = computed(() => [
-  `content/az-104.pdf`,
-  `content/az-900.pdf`
+  {
+    file: `images/Certs_2.png`,
+    url: 'https://learn.microsoft.com/api/credentials/share/en-us/nvtung/C1C5BBCDD21770C3?sharingId=5F11ACC34CA524D'
+  },
+  {
+    file: `images/Certs_1.png`,
+    url: 'https://learn.microsoft.com/api/credentials/share/en-us/nvtung/6A08F9787FA7B1F4?sharingId=5F11ACC34CA524D'
+  },
 ])
 
 const credly = computed(() => [
   'b50d2d25-4689-4489-bc3b-440e985a5128'
 ])
+
+const efsetUrl = computed(() => {
+  let lang = localeIndex.value != 'root' ? (localeIndex.value + '/') : '';
+  return `https://cert.efset.org/${lang}Yr64ri`
+});
 
 onMounted(async () => {
   await nextTick();
@@ -37,35 +58,31 @@ onMounted(async () => {
 
 <template>
   <div class="cert">
-    <iframe :src="efsetUrl" style="height: 840px; width: 100%; margin-bottom: 1.6rem;"></iframe>
+    <a v-for="cert in certificates" :href="cert.url" 
+      target="_blank" 
+      rel="noopener noreferrer">
+      <img :src="cert.file" class="certification" style="width: 100%; margin-bottom: 1.6rem;">
+    </a>
 
-    <img v-for="cert in certificates" class="certification" :src="cert" style="width: 100%; margin-bottom: 1.6rem;">
-
-    <div v-for="pdf in microsoft" class="pdf-container">
-      <iframe :src="`${pdf}#page=1&toolbar=0&navpanes=0&scrollbar=0&view=FitH`" class="pdf"></iframe>
-    </div>
+    <a v-for="cert in microsoft" :href="cert.url" 
+      target="_blank" 
+      rel="noopener noreferrer">
+      <img :src="cert.file" class="certification" style="width: 100%; margin-bottom: 1.6rem;">
+    </a>
 
     <div class="credly">
       <div v-for="cert in credly" class="certification" data-iframe-width="330" data-iframe-height="265" :data-share-badge-id="cert" data-share-badge-host="https://www.credly.com"></div>
     </div>
+
+    <iframe :src="efsetUrl" style="height: 840px; width: 100%; margin-bottom: 1.6rem;"></iframe>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .cert {
-  .pdf-container {
-    width: 100%;
-    margin: 0 auto; margin-bottom: 1.6rem;
-    aspect-ratio: 11 / 7.7;
-    position: relative;
-    overflow: hidden;
-    .pdf {
-      position: absolute; top: 0; left: 0;
-      width: calc(100% + 20px); height: 100%;
-    }
-  }
   .credly {
     display: flex; gap: 1.6rem; justify-content: center;
+    width: 100%; margin-bottom: 1.6rem;
     @media (max-width: 720px) {
       flex-direction: column; align-items: center;
     }
